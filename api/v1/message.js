52f243dc4e5555f0f3cf775ea31ca41aac33d0b7
bing.kong <bingkong@bingkongs-MacBook-Pro.local>
2016-04-28 10:06:11 +0800

    modify weixin ack

--- api/v1/message.js
+++ api/v1/message.js
@@ -1,78 +1,84 @@
 var express = require('express');
 var config = require('../../config');
 var utility = require('utility');
-//var auth = require('node-weixin-auth');
-//var errors = require('web-errors').error;
-//var settings = require('node-weixin-settings');
+var auth = require('node-weixin-auth');
+var errors = require('web-errors').error;
+var settings = require('node-weixin-settings');
 
 //wx 消息接口
 exports.index = function(req,res,next){
+    var app = {
+        id:config.app_id,
+        secret:config.app_secrect,
+        token:config.token
+    };
+
+    //var data = auth.extra(req,body);
+    var data = {
+        signature:req.query.signature,
+        timestamp:req.query.timestamp,
+        nonce:req.query.nonce,
+        echostr:req.query.echostr
+    };
+
+    auth.ack(app.token,data,function(err,data){
+        if(!err){
+            res.send(data);
+            return;
+        }
+
+        switch(err){
+            case 1:
+                res.send(errors.INPUT_INVALID);
+                break;
+            case 2:
+                res.send(errors.SIGNATURE_NOT_MATCH);
+                break;
+            default:
+                res.send(errors.UNKNOWN_ERROR);
+                break;
+        }
+    });
+
 
     /*
-       var app = {
-       id:config.app_id,
-       secret:config.app_secrect,
-       token:config.token
-       };
 
-       var data = auth.extra(req,body);
+    auth.tokenize(settings,app,function(err,json){
+        var accessToken = json.access_token;
+    });
 
-       auth.ack(app.token,data,function(err,data){
-       if(!err){
-       res.send(data);
-       return;
-       }
+    var messages = nodeWeixinMessage.messages;
+    function text(message,res,callback,extra){
+        console.log(message);
+        console.log("text")
+    }
 
-       switch(err){
-       case 1:
-       res.send(errors.INPUT_INVALID);
-       break;
-       case 2:
-       res.send(errors.SIGNATURE_NOT_MATCH);
-       break;
-       default:
-       res.send(errors.UNKNOWN_ERROR);
-       break;
-       }
-       });
+    messages.on.text(text);
+    messages.onXML(req.body,res,function callback(message){
+        console.log("onXML")
+    });
 
-       auth.tokenize(settings,app,function(err,json){
-       var accessToken = json.access_token;
-       });
+    messages.subscribe(function(message){
+        console.log(message);
+    });
+       var signature = req.query.signature;
+       var timestamp = req.query.timestamp;
+       var nonce = req.query.nonce;
+       var echostr = req.query.echostr;
+       var token = config.token;
 
-       var messages = nodeWeixinMessage.messages;
-       function text(message,res,callback,extra){
-       console.log(message);
-       console.log("text")
-       }
+       var tmpArr=[token,timestamp,nonce];
 
-       messages.on.text(text);
-       messages.onXML(req.body,res,function callback(message){
-       console.log("onXML")
-       });
+       tmpArr.sort();
 
-       messages.subscribe(function(message){
-       console.log(message);
-       });
+       var tmpStr = tmpArr.join('');
 
-*/
-    var signature = req.query.signature;
-    var timestamp = req.query.timestamp;
-    var nonce = req.query.nonce;
-    var echostr = req.query.echostr;
-    var token = config.token;
+       tmpStr = utility.sha1(tmpStr);
 
-    var tmpArr=[token,timestamp,nonce];
-
-    tmpArr.sort();
-
-    var tmpStr = tmpArr.join('');
-
-    tmpStr = utility.sha1(tmpStr);
-
-    if(tmpStr === signature)
-        res.send(echostr);
-    else 
-        res.send('error');
+       if(tmpStr === signature)
+       res.send(echostr);
+       else 
+       res.send('error');
+       */
 };
 
