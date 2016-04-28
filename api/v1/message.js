d5f53fb8829c7a4ec91826dd207a737cca3e2f79
bing.kong <bingkong@bingkongs-MacBook-Pro.local>
2016-04-28 09:15:47 +0800

    weixin message use node-wx-message

--- api/v1/message.js
+++ api/v1/message.js
@@ -1,27 +1,47 @@
 var express = require('express');
 var config = require('../../config');
 var utility = require('utility');
+var nodeWeixinMessage = require('node-weixin-message');
+
 
 //wx 消息接口
 exports.index = function(req,res,next){
- 
-    var signature = req.query.signature;
-    var timestamp = req.query.timestamp;
-    var nonce = req.query.nonce;
-    var echostr = req.query.echostr;
-    var token = config.token;
 
-    var tmpArr=[token,timestamp,nonce];
+    var messages = nodeWeixinMessage.messages;
+    function text(message,res,callback,extra){
+        console.log(message);
+        console.log("text")
+    }
 
-    tmpArr.sort();
+    messages.on.text(text);
+    messages.onXML(req.body,res,function callback(message){
+        console.log("onXML")
+    });
 
-    var tmpStr = tmpArr.join('');
-    
-    tmpStr = utility.sha1(tmpStr);
+    messages.subscribe(function(message){
+        console.log(message);
+    });
 
-    if(tmpStr === signature)
-        res.send(echostr);
-    else 
-        res.send('error');
+    /*
+
+       var signature = req.query.signature;
+       var timestamp = req.query.timestamp;
+       var nonce = req.query.nonce;
+       var echostr = req.query.echostr;
+       var token = config.token;
+
+       var tmpArr=[token,timestamp,nonce];
+
+       tmpArr.sort();
+
+       var tmpStr = tmpArr.join('');
+
+       tmpStr = utility.sha1(tmpStr);
+
+       if(tmpStr === signature)
+       res.send(echostr);
+       else 
+       res.send('error');
+       */
 };
 
--- test.js
+++ test.js
@@ -1,6 +1,19 @@
 
 
-var member = require('./model');
+var t = function(){
+    if(arguments)
+    {
+        for(var i=0; i< arguments.length;i++)
+        {
+            console.log(arguments[i]);
+        }
+    }
+};
+
+
+var arr=["aa","bb",11];
+
+t.apply(null,arr);
 
 
 
