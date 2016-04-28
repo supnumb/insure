a543077e13d9fae1daf1f3ce9859635848c6a4df
bing.kong <bingkong@bingkongs-MacBook-Pro.local>
2016-04-28 09:56:50 +0800

    modify weixin ack

--- api/v1/message.js
+++ api/v1/message.js
@@ -1,85 +1,78 @@
 var express = require('express');
 var config = require('../../config');
 var utility = require('utility');
-var auth = require('node-weixin-auth');
-var errors = require('web-errors').error;
+//var auth = require('node-weixin-auth');
+//var errors = require('web-errors').error;
 //var settings = require('node-weixin-settings');
-
 
 //wx 消息接口
 exports.index = function(req,res,next){
 
-    var app = {
-        id:config.app_id,
-        secret:config.app_secrect,
-        token:config.token
-    };
-
-    var data = auth.extra(req,body);
-
-    auth.ack(app.token,data,function(err,data){
-    if(!err){
-    res.send(data);
-    return;
-    }
-
-    switch(err){
-        case 1:
-            res.send(errors.INPUT_INVALID);
-            break;
-        case 2:
-            res.send(errors.SIGNATURE_NOT_MATCH);
-            break;
-        default:
-            res.send(errors.UNKNOWN_ERROR);
-            break;
-    }
-    });
     /*
+       var app = {
+       id:config.app_id,
+       secret:config.app_secrect,
+       token:config.token
+       };
 
+       var data = auth.extra(req,body);
 
-    auth.tokenize(settings,app,function(err,json){
-    
-    var accessToken = json.access_token;
+       auth.ack(app.token,data,function(err,data){
+       if(!err){
+       res.send(data);
+       return;
+       }
 
+       switch(err){
+       case 1:
+       res.send(errors.INPUT_INVALID);
+       break;
+       case 2:
+       res.send(errors.SIGNATURE_NOT_MATCH);
+       break;
+       default:
+       res.send(errors.UNKNOWN_ERROR);
+       break;
+       }
+       });
 
-    
-    });
+       auth.tokenize(settings,app,function(err,json){
+       var accessToken = json.access_token;
+       });
 
+       var messages = nodeWeixinMessage.messages;
+       function text(message,res,callback,extra){
+       console.log(message);
+       console.log("text")
+       }
 
-    var messages = nodeWeixinMessage.messages;
-    function text(message,res,callback,extra){
-        console.log(message);
-        console.log("text")
-    }
+       messages.on.text(text);
+       messages.onXML(req.body,res,function callback(message){
+       console.log("onXML")
+       });
 
-    messages.on.text(text);
-    messages.onXML(req.body,res,function callback(message){
-        console.log("onXML")
-    });
+       messages.subscribe(function(message){
+       console.log(message);
+       });
 
-    messages.subscribe(function(message){
-        console.log(message);
-    });
+*/
+    var signature = req.query.signature;
+    var timestamp = req.query.timestamp;
+    var nonce = req.query.nonce;
+    var echostr = req.query.echostr;
+    var token = config.token;
 
-       var signature = req.query.signature;
-       var timestamp = req.query.timestamp;
-       var nonce = req.query.nonce;
-       var echostr = req.query.echostr;
-       var token = config.token;
+    var tmpArr=[token,timestamp,nonce];
 
-       var tmpArr=[token,timestamp,nonce];
+    tmpArr.sort();
 
-       tmpArr.sort();
+    var tmpStr = tmpArr.join('');
 
-       var tmpStr = tmpArr.join('');
+    tmpStr = utility.sha1(tmpStr);
 
-       tmpStr = utility.sha1(tmpStr);
-
-       if(tmpStr === signature)
-       res.send(echostr);
-       else 
-       res.send('error');
-       */
+    if(tmpStr === signature)
+        res.send(echostr);
+    else 
+        res.send('error');
 };
 
